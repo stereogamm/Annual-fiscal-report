e56b3d132e84bd2cfc538e9d6c2557c9b371a4dc
Marina Sysoliatina <stereogam@yandex.ru>
2025-03-27 01:01:30 +0100

    add form component, fix favicon, add form styles, add background

--- src/Components/Costs/CostItem.jsx
+++ src/Components/Costs/CostItem.jsx
@@ -1,14 +1,23 @@
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
-
+import { useState } from 'react';
 import "./costItem.css";
 
 function CostItem(props) {
+
+const[item, setItem] = useState(props.item);
+
+
+const changeItemHandler = () => {
+  setItem('new item');
+}
+
   return (
     <Card className="cost-item">
       <CostDate dante={props.date} />
-      <h3 className="cost-item__name">{props.item}</h3>
+      <h3 className="cost-item__name">{item}</h3>
       <div className="cost-item__summ">{props.summa}btc</div>
+      <button onClick={changeItemHandler}>Change item</button>
     </Card>
   );
 }
